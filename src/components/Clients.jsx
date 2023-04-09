c4e527c75b2c31e623a5edd152c63e5c115994cc
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2023-04-10 01:01:20 +0600

    feedback and clients section done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { clients } from "../constants";
+import styles from "../styles";
 
 const Clients = () => {
   return (
-    <div>
-      Clients
-    </div>
+    <section id="clients" className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((item, index) => (
+          <div key={index} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}>
+            <img
+              src={item.logo}
+              alt={item.name}
+              className="sm:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
